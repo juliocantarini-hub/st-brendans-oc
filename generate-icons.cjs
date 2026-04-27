af2fd3177fead8617c0c8f0c967cadfebd0d47b6
juliocantarini-hub <julio.cantarini@gmail.com>
2026-04-27 08:25:20 -0300

    Actualizar iconos PWA

--- generate-icons.cjs
+++ generate-icons.cjs
@@ -3,9 +3,8 @@ const sharp = require('sharp')
 const svg = `
 <svg width="512" height="512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
   <rect width="512" height="512" rx="0" fill="#0F6E56"/>
-  <circle cx="220" cy="360" r="60" fill="#9FE1CB"/>
-  <rect x="270" y="100" width="28" height="280" fill="#9FE1CB" rx="14"/>
-  <path d="M298 100 Q420 140 400 230 Q380 300 298 280" fill="#9FE1CB"/>
+  <g transform="translate(106, 106) scale(12.5)" fill="#9FE1CB">   
+  <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/> </g>
 </svg>
 `
 
